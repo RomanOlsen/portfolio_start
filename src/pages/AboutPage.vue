d75a64a62f9526a06c633e746b58eb4b8bf6fefc
RomanOlsen <romanjolsen@gmail.com>
2025-05-11 16:49:21 -0600

    adding columns and imgfluid

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -14,10 +14,12 @@
             building games using Roblox Studio/Unity/Scratch, 3D modeling in Blender, logo design with Adobe
             Illustrator/GIMP/Canva, video editing, and even building in Minecraft, I've always been curious to explore
             whats out there, and how what I make might solve a problem.</p>
-          <p>I'm also an outdoor enthusiast. I love walking along the foothills in my city, Boise. And always down for a hike
+          <p>I'm also an outdoor enthusiast. I love walking along the foothills in my city, Boise. And always down for a
+            hike
             wherever I'm traveling. Love running, specifically sprinting. Love the adrenaline rush.
           </p>
-          <p>I am a firm believer in both technology and nature and how they can go hand-in-hand. I strongly believe the importance of coding hard, and resting hard!</p>
+          <p>I am a firm believer in both technology and nature and how they can go hand-in-hand. I strongly believe the
+            importance of coding hard, and resting hard!</p>
           <!-- <p>I am a firm believer in both technology and nature and how they can go hand-in-hand. I strongly believe the importance of coding hard, and resting hard. I believe that whatever I build or contribute in the future will be for the benefit of humanity.</p> -->
 
           <ul>
@@ -33,34 +35,45 @@
 
 
         </div>
-        <div class="col-12">
-          <h2 class="text-center py-3">Education</h2>
-          <div class="d-flex justify-content-center align-items-center gap-5">
+        <h2 class="text-center py-3">Education</h2>
+        <!-- <hr> -->
 
-            <div>
-              <img class="education" src="C:\Users\Roman\source\codeworks\May\portfolio_start\src\assets\img\cw-logo-circle.png"
-                alt="CodeWorks">
-              <div class="text-center">
-                CodeWorks
-              </div>
-            </div>
-            <div>
-  
-              <img class="education" src="C:\Users\Roman\source\codeworks\May\portfolio_start\src\assets\img\OneStone+-+Stacked.png"
-                alt="One Stone">
-                <div class="text-center">
-                  One Stone
-                </div>
-            </div>
+        <div class="col-6 text-center">
+
+
+
+          <img class="education img-fluid"
+            src="C:\Users\Roman\source\codeworks\May\portfolio_start\src\assets\img\cw-logo-circle.png" alt="CodeWorks">
+          <div class="pt-2">
+            <a href="https://boisecodeworks.com/" target="_blank" rel="noopener noreferrer"
+              title="View my software development school">CodeWorks
+            </a>
+
           </div>
         </div>
+
+        <div class="col-6 text-center">
+
+          <img class="education img-fluid"
+            src="C:\Users\Roman\source\codeworks\May\portfolio_start\src\assets\img\OneStone+-+Stacked.png"
+            alt="One Stone">
+          <div class="pt-2">
+            <a href="https://onestone.org/" target="_blank" rel="noopener noreferrer"
+              title="View my highschool i went to">One Stone
+            </a>
+
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
+
 </template>
 
 <style scoped lang="scss">
 .education {
-  height: 10dvh;
+  height: 20dvh;
+  object-fit: cover;
 }
 </style>
